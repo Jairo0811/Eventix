4477494e2ee37fb060db0c367098e1873f16006a
Jairo Matías <jairo_1829@hotmail.com>
2026-08-10 10:28:41 -0400

    test(notification): agregar validación básica

---
+++ src/test/java/com/jairomatias/eventix/notification/service/NotificationServiceBasicTest.java
@@ -0,0 +1,13 @@
+package com.jairomatias.eventix.notification.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class NotificationServiceBasicTest {
+
+    @Test
+    void serviceShouldBeAvailable() {
+        assertNotNull(NotificationService.class);
+    }
+}
