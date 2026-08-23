359999a60d2c2f8aaf1937b91be6d44023c10e39
Jairo Matías <jairo_1829@hotmail.com>
2026-08-23 12:33:48 -0400

    feat(notification): add internal notification types

---
+++ src/main/java/com/jairomatias/eventix/notification/entity/InternalNotificationType.java
@@ -0,0 +1,11 @@
+package com.jairomatias.eventix.notification.entity;
+
+public enum InternalNotificationType {
+    PURCHASE,
+    REFUND,
+    TICKET,
+    SETTLEMENT,
+    ELIGIBILITY,
+    REMINDER,
+    SYSTEM
+}
