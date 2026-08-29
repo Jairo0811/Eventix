b7be018ea9dd2c45b16c29933852c78bb5ec0843
Jairo Matías <jairo_1829@hotmail.com>
2026-08-29 13:01:02 -0400

    feat(v1.3.3): add identity provider unavailable exception

---
+++ src/main/java/com/jairomatias/eventix/eligibility/identity/CitizenIdentityProviderUnavailableException.java
@@ -0,0 +1,8 @@
+package com.jairomatias.eventix.eligibility.identity;
+
+public class CitizenIdentityProviderUnavailableException extends RuntimeException {
+
+    public CitizenIdentityProviderUnavailableException(String message) {
+        super(message);
+    }
+}
