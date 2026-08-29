28b75582cea2d0e9a55e946be0292038ade16448
Jairo Matías <jairo_1829@hotmail.com>
2026-08-29 13:05:52 -0400

    refactor(v1.3.3): lock legacy standalone pages to hybrid theme

---
+++ src/main/resources/static/js/theme.js
@@ -0,0 +1,7 @@
+(() => {
+    "use strict";
+    const root = document.documentElement;
+    root.dataset.theme = "hybrid";
+    root.setAttribute("data-bs-theme", "light");
+    root.style.colorScheme = "light";
+})();
